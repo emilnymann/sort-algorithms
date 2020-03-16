28bcca92b21c07171ab5e5bc4c9c1cb74ab9851c
Emil Nymann <emilnymann96@gmail.com>
2020-03-16 14:26:24 +0100

    Lavet selection sort

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,20 @@ public class Main {
         System.out.println("Sorted data with bubble sort:");
         System.out.println(data);
 
+        System.out.println("-----------------------------------------------");
+
+        data.clear();
+        for (int i = 0; i < 10; i++) {
+            int rnd = ThreadLocalRandom.current().nextInt(0, 100 + 1);
+            data.add(rnd);
+        }
+
+        System.out.println("Unsorted data:");
+        System.out.println(data);
+        selectionSort();
+        System.out.println("Sorted data with selection sort:");
+        System.out.println(data);
+
     }
 
     static void bubbleSort() {
@@ -41,5 +55,33 @@ public class Main {
                 System.out.println(data.get(i) + " is larger than " + data.get(i - 1) + "... Moving on.");
             }
         }
+    }
+
+    static void selectionSort() {
+        int sortMin = 0;
+
+        while (sortMin < data.size()) {
+
+            int lowestIndex = sortMin;
+
+            for (int i = sortMin; i < data.size(); i++) {
+                if (data.get(i) < data.get(lowestIndex)) {
+                    System.out.println(data.get(i) + " is lowest number so far.");
+                    lowestIndex = i;
+                }
+                else {
+                    System.out.println(data.get(i) + " is not lower than " + data.get(lowestIndex) + ", moving on.");
+                }
+            }
+
+            System.out.println("Swapping " + data.get(lowestIndex) + " to the lowest unsorted index, marking it as sorted and moving on.");
+            int temp = data.get(lowestIndex);
+            data.set(lowestIndex, data.get(sortMin));
+            data.set(sortMin, temp);
+
+            sortMin++;
+
+        }
+
     }
 }
